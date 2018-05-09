36a617c39f9b052201e2b44b1226d65ff17264d5
Vladimir <Leamhein@hotmail.com>
2018-05-09 21:37:16 +0300

    Add new code

--- script/chart.js
+++ script/chart.js
@@ -1,20 +1,39 @@
 window.onload = () => {
-    let bars = d3.selectAll(".bar"); /* Select all bars */
-    console.log(bars); /* [Array(5)] */
-    /*  bars.style("height", "40px"); /* Change charts height to 40px */
+    
+    d3.select("#bars").append("h3").text("Bars");
+    
+    d3.select("h3").remove("h3");
+   
+    d3.select("#bars").insert("h3", "div").text("Bars");
+    
+    d3.select("h3").style("color", "blue").style("width", "400px").style("border", "1px solid red").style("margin", "0 auto");
+    
+    d3.select("#bars").select("div").attr("class", "charts");
 
-    let numbers = [100, 50, 300, 120, 260]; /* Data array */
-    bars.data(numbers); /* connect the data with the bars */
+
+    let numbers = [100, 50, 25, 120, 250, 100, 170, 502]; /* Data array */
+
+
+    /*let bars = d3.selectAll(".bar"); /* Select all bars */
+    /*bars.data(numbers); /* connect the data with the bars */
     /* d3.selectAll(".bar").data(numbers); /* chaining in D3 */
-    bars.style("height", function (d) {
-        return d + "px";
-    })
-    bars.style("margin-top", (d) => {
-        console.log(d);
-        return 569 - d + 'px';
-    })
+    /*bars.text((d) => {
+        return d;
+    })*/
 
-    bars.text((d) => {
-        return d
-    })
+    /*enter*/
+    d3.select("div").selectAll(".bar").data(numbers).enter().append('div').attr("class", "bar");
+    
+    /*update*/
+    d3.selectAll(".bar").data(numbers).style("height", function (d) {
+        return d + "px";
+    }).text((d) => {
+        return d;
+    });
+    
+    /*exit*/
+    d3.selectAll(".bar").data(numbers).exit().remove();
+
+
+
 }

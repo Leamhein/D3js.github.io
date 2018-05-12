8a949a68dc3ba151566a670c6b8470eebd5501ba
Vladimir <Leamhein@hotmail.com>
2018-05-12 12:51:19 +0300

    All done

--- script/chart.js
+++ script/chart.js
@@ -11,11 +11,25 @@ window.onload = () => {
         .style("align-items", "flex-end")
         .style("justify-content", "space-between");
 
-    d3.select("#bar-chart").append("div").attr("class", "bar").style("background", "#003366").style("width", "50px");
+    d3.selectAll(".bar").attr("class", "bar").style("background", "#003366").style("width", "50px");
 
     let arr = [150, 270, 64, 402, 50];
-
+    /*
     d3.selectAll(".bar").data(arr).style("height", function (d) {
         return d + "px";
     });
+    */
+
+    d3.selectAll(".bar").data(arr).enter().select("#bar-chart").append("div").attr("class", "bar").style("background", "#003366").style("width", "50px");
+
+    d3.selectAll(".bar")
+        .data(arr)
+        .style("height", (d) => {
+            return d + "px";
+        })
+        .text((d) => {
+            return d;
+        }).style("color", "white");
+
+    d3.selectAll(".bar").data(arr).exit().remove();
 }
